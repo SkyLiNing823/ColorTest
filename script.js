0b38e40fdeea27866f4079e72c76be2a6a6fa2db
上傳人名字 <flamesky823@gmail.com>
2022-06-14 20:15:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -127,11 +127,15 @@ let soundeffect = true;
 let bgm = true;
 
 
+
+//點擊異色色塊
 function clickansBtn() {
+    //檢查音效是否啟用
     if (soundeffect == true) {
         soundEffect_ans.currentTime = 0;
         soundEffect_ans.play();
     }
+    //變更狀態列
     Floor = parseInt(floorNum.innerText);
     Score = parseInt(scoreNum.innerText);
     Floor += 1;
@@ -147,20 +151,27 @@ function clickansBtn() {
     if (count < 20)
         count++;
     board.innerHTML = "";
+    //生成新版面
     createBlocks(count);
 }
 
+//點擊同色色塊
 function clickotherBtn() {
+    //檢查音效是否啟用
     if (soundeffect == true) {
         soundEffect_break.currentTime = 0;
         soundEffect_break.play();
     }
+    //扣血
     hp--;
+    //調整生命值版面
     heartControll(hp);
     remainHP.innerText = "0" + hp;
-    if (hp == 1)
+    //當生命值剩下1時停用提示功能
+    if (hp == 1) {
         hintIcon.src = "resources/hint_lock.png";
-    else if (hp == 0) {
+        //當生命值剩下0時結束遊戲並結算
+    } else if (hp == 0) {
         startIcon.src = "resources/start.png";
         clearInterval(countdown);
         clearTimeout(timeout);
@@ -171,11 +182,13 @@ function clickotherBtn() {
         alert(`您已用盡所有生命值！您已通過: ${Floor-1} 關，總共得到: ${Score} 分！`);
         board.innerHTML = "";
         gameStart = false;
+        //全程無使用暫停功能則紀錄分數
         if (record == true)
             recordAdd();
     }
 }
 
+//時間用盡之結算
 function timeOut() {
     startIcon.src = "resources/start.png";
     floorNum.innerText = "00";
@@ -183,10 +196,12 @@ function timeOut() {
     alert(`時間到！您已通過: ${Floor-1} 關，總共得到: ${Score} 分！`);
     board.innerHTML = "";
     gameStart = false;
+    //全程無使用暫停功能則紀錄分數
     if (record == true)
         recordAdd();
 }
 
+//倒數計時
 function countDown() {
     if (Sec > 0) {
         Sec--;
@@ -194,6 +209,7 @@ function countDown() {
             remainSec.innerText = "0" + Sec;
         else
             remainSec.innerText = Sec;
+        //分秒換算
     } else if (Min > 0 && Sec == 0) {
         Min--;
         Sec = 59;
@@ -206,6 +222,7 @@ function countDown() {
 
 }
 
+//控制版面hp貼圖
 function heartControll(hp) {
     for (let i = 0; i < 5; i++) {
         if (i < hp)
@@ -215,10 +232,34 @@ function heartControll(hp) {
     }
 }
 
+//根據通過關卡數控制色差(難易度)
+function randomControll() {
+    //10關以內，色差控制在20~35
+    if (Floor < 10) {
+        return Math.floor(Math.random() * 15) + 20;
+        //10~19關，色差控制在15~30
+    } else if (Floor < 20) {
+        return Math.floor(Math.random() * 15) + 15;
+        //20~29關，色差控制在10~25
+    } else if (Floor < 30) {
+        return Math.floor(Math.random() * 15) + 10;
+        //30~39關，色差控制在5~20
+    } else if (Floor < 40) {
+        return Math.floor(Math.random() * 15) + 5;
+        //40關以後，色差控制在5~15
+    } else {
+        return Math.floor(Math.random() * 10) + 5;
+    }
+}
+
+//生成num*num版面
 function createBlocks(num) {
+    //版面淨空
     board.innerHTML = "";
+    //隨機決定異色方塊之行列
     ansRow = Math.floor(Math.random() * num) + 1;
     ansCol = Math.floor(Math.random() * num) + 1;
+    //生成方塊
     for (let i = 1; i < num + 1; i++) {
         for (let j = 1; j < num + 1; j++) {
             if (i == ansRow && j == ansCol)
@@ -227,6 +268,7 @@ function createBlocks(num) {
                 board.innerHTML += `<button class="pressBtn otherBtn" id="${i}x${j}"></button>`;
         }
     }
+    //方塊行列數大於3且生命值大於1可使用提示功能
     if (num > 3) {
         if (hp > 1)
             hintIcon.src = "resources/hint.png";
@@ -236,17 +278,23 @@ function createBlocks(num) {
     ansBtn = document.getElementById("ansBtn");
     pressBtn = document.getElementsByClassName("pressBtn");
     otherBtn = document.getElementsByClassName("otherBtn");
+    //自動調整方塊邊長
     blockLength = 650 / count;
+    //決定同色方塊之RGB，各介於50~210
     R = Math.floor(Math.random() * 160) + 50;
     G = Math.floor(Math.random() * 160) + 50;
     B = Math.floor(Math.random() * 160) + 50;
-    mode = Math.floor(Math.random() * 8);
-    Array.from(pressBtn).forEach(function(pressBtn) { //for every pressBtn
+    //隨機決定異色模式
+    mode = Math.floor(Math.random() * 15);
+    //將設定套用於所有方塊
+    Array.from(pressBtn).forEach(function(pressBtn) {
         pressBtn.style.width = `${blockLength}px`;
         pressBtn.style.height = `${blockLength}px`;
         pressBtn.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
     });
-    randomNum = Math.floor(Math.random() * 10) + 15;
+    //得到色差值
+    randomNum = randomControll();
+    //將異色方塊上色
     switch (mode) {
         case 0:
             ansBtn.style.backgroundColor = `rgb(${R-randomNum}, ${G-randomNum}, ${B-randomNum})`;
@@ -272,25 +320,53 @@ function createBlocks(num) {
         case 7:
             ansBtn.style.backgroundColor = `rgb(${R+randomNum}, ${G+randomNum}, ${B+randomNum})`;
             break;
+        case 8:
+            ansBtn.style.backgroundColor = `rgb(${R}, ${G+randomNum}, ${B+randomNum})`;
+            break;
+        case 9:
+            ansBtn.style.backgroundColor = `rgb(${R+randomNum}, ${G}, ${B+randomNum})`;
+            break;
+        case 10:
+            ansBtn.style.backgroundColor = `rgb(${R+randomNum}, ${G+randomNum}, ${B})`;
+            break;
+        case 11:
+            ansBtn.style.backgroundColor = `rgb(${R}, ${G+randomNum}, ${B+randomNum})`;
+            break;
+        case 12:
+            ansBtn.style.backgroundColor = `rgb(${R}, ${G-randomNum}, ${B-randomNum})`;
+            break;
+        case 13:
+            ansBtn.style.backgroundColor = `rgb(${R-randomNum}, ${G}, ${B-randomNum})`;
+            break;
+        case 14:
+            ansBtn.style.backgroundColor = `rgb(${R-randomNum}, ${G-randomNum}, ${B})`;
+            break;
     }
+    //將異色方塊綁定點擊函式
     ansBtn.addEventListener("click", clickansBtn)
+        //將同色方塊綁定點擊函式
     for (let i = 0; i < otherBtn.length; i++) {
         otherBtn[i].addEventListener("click", clickotherBtn);
     }
 }
 
+//將紀錄印在計分板上
 function recordAdd() {
     scoreContent.innerHTML += `<p> > ${recordMin}m ${recordSec}s 內，通過 ${Floor-1} 關 得到 ${Score} 分</p>`
 }
 
+//點擊開始按鈕
 startBtn.addEventListener("click", function() {
-    record = true;
-    gameStart = true;
+    //檢查BGM是否啟用
     if (bgm == true)
         BGM.play();
+    //重置狀態
+    record = true;
+    gameStart = true;
+    pause = false;
+    //更換按鈕貼圖、設置狀態列
     startIcon.src = "resources/reset.png";
     pauseIcon.src = "resources/pause.png";
-    pause = false;
     remainHP.innerText = "05";
     floorNum.innerText = "01";
     scoreNum.innerText = "00";
@@ -314,63 +390,86 @@ startBtn.addEventListener("click", function() {
     heartControll(hp);
     clearTimeout(timeout);
     clearInterval(countdown);
+    //設定倒數
     timeout = setTimeout(timeOut, (Min * 60 + Sec) * 1000 + 50);
     countdown = setInterval(countDown, 1000);
     count = 2;
     board.innerHTML = "";
+    //製作新版面
     createBlocks(count);
 })
 
+//點擊暫停按鈕
 pauseBtn.addEventListener("click", function() {
+    //遊戲開始才可使用
     if (gameStart == true) {
         Min = parseInt(remainMin.innerText);
         Sec = parseInt(remainSec.innerText);
         if (pause == false) {
+            //暫停倒數
             clearTimeout(timeout);
             clearInterval(countdown);
+            //移除方塊點擊函式以鎖定版面
             ansBtn.removeEventListener("click", clickansBtn);
             for (let i = 0; i < otherBtn.length; i++) {
                 otherBtn[i].removeEventListener("click", clickotherBtn);
             }
+            //按鈕貼圖更換
             pauseIcon.src = "resources/play.png";
             pause = true;
+            //將該局設為不可紀錄
             record = false;
         } else {
+            //解除暫停
             timeout = setTimeout(timeOut, (Min * 60 + Sec) * 1000 + 50);
             countdown = setInterval(countDown, 1000);
+            //重綁方塊點擊函式以回復版面
             ansBtn.addEventListener("click", clickansBtn)
             for (let i = 0; i < otherBtn.length; i++) {
                 otherBtn[i].addEventListener("click", clickotherBtn);
             }
+            //按鈕貼圖更換
             pauseIcon.src = "resources/pause.png";
             pause = false;
         }
     }
 })
 
+//點擊音效按鈕
 soundEffectBtn.addEventListener("click", function() {
+    //關音效
     if (soundeffect == true) {
+        //按鈕貼圖更換
         soundEffectIcon.src = "resources/soundEffect_off.png";
         soundeffect = false;
+        //開音效
     } else {
+        //按鈕貼圖更換
         soundEffectIcon.src = "resources/soundEffect_on.png";
         soundeffect = true;
     }
 })
 
+//點擊BGM按鈕
 BGMBtn.addEventListener("click", function() {
+    //關BGM
     if (bgm == true) {
         BGM.pause();
+        //按鈕貼圖更換
         BGMIcon.src = "resources/BGM_off.png";
         bgm = false;
+        //開BGM
     } else {
         BGM.play();
+        //按鈕貼圖更換
         BGMIcon.src = "resources/BGM_on.png";
         bgm = true;
     }
 })
 
+//點擊提示按鈕
 hintBtn.addEventListener("click", function() {
+    //判斷可否使用提示
     if (gameStart == true && hintUsed == false && count > 3) {
         if (hp > 1) {
             if (soundeffect == true) {
@@ -379,8 +478,10 @@ hintBtn.addEventListener("click", function() {
             }
             hintUsed = true;
             hintIcon.src = "resources/hint_lock.png";
+            //扣血
             hp--;
             heartControll(hp);
+            //圈定Row範圍
             if (ansRow == 1) {
                 rowStart = 1;
                 rowEnd = 3;
@@ -391,6 +492,7 @@ hintBtn.addEventListener("click", function() {
                 rowStart = ansRow - 1;
                 rowEnd = ansRow + 1;
             }
+            //圈定Col範圍
             if (ansCol == 1) {
                 colStart = 1;
                 colEnd = 3;
@@ -401,6 +503,7 @@ hintBtn.addEventListener("click", function() {
                 colStart = ansCol - 1;
                 colEnd = ansCol + 1;
             }
+            //畫出九宮格
             for (let i = 1; i <= count; i++) {
                 for (let j = 1; j <= count; j++) {
                     if ((i < rowStart || i > rowEnd) || (j < colStart || j > colEnd)) {
